d6bbf6e725387ce649c176ff37ffce0df56ff01b
sofs-00 <75382159+sofs-00@users.noreply.github.com>
2020-12-02 16:19:09 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -6,9 +6,9 @@
 
 /* 
  * File:   main.c
- * Author: Super Admin
+ * Author: Sofia
  *
- * Created on 2 de dezembro de 2020, 16:12
+ * Created on 08 November 2020, 19:11
  */
 
 #include <stdio.h>
@@ -19,6 +19,27 @@
  */
 int main(int argc, char** argv) {
 
+    int cifra, i;
+    char nome[10];
+    printf("diga o seu nome: ");
+    scanf("%s", nome);
+    printf("diga o ultimo digito do seu aniversario: ");
+    scanf("%d", &cifra);
+    printf("o seu nome na verdade é: ");
+    for ( i=0 ; nome[i]!='\0' ; i++) {
+        if ( nome[i]>=97 && nome[i]<=122) {
+            if ( nome[i]+cifra>122) {
+                nome[i]-=26;
+            }
+        }
+        else {
+            if ( nome[i]+cifra>90) {
+                nome[i]-=26;
+            }
+        }
+        printf("%c", nome[i]+cifra);
+    }
+    
     return (EXIT_SUCCESS);
 }
 
