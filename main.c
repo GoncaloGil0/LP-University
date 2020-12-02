845172ec85dedf307e9ad926a56930e53cd1b1f1
Gonçalo Gil <43255636+GoncaloGil0@users.noreply.github.com>
2020-12-02 16:14:20 +0000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Super Admin
+ *
+ * Created on 2 de dezembro de 2020, 16:12
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
