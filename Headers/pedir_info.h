49d20af31d13f01463a24acba808e80a14088f08
sofs-00 <75382159+sofs-00@users.noreply.github.com>
2020-12-09 17:31:14 +0000

    Update pedir_info.h

--- Headers/pedir_info.h
+++ Headers/pedir_info.h
@@ -4,26 +4,28 @@
 #define PEDIR_INFO_H
 
 typedef enum {
-        CASADO, SOLTEIRO, DIVORCIADO, VIUVO
-    } Est_Civil;
+    CASADO, SOLTEIRO, DIVORCIADO, VIUVO
+} Est_Civil;
 
-    typedef enum {
-        EMPREGADO, CHEFE, ADMINISTRADOR
-    } Cargo;
+typedef enum {
+    EMPREGADO, CHEFE, ADMINISTRADOR
+} Cargo;
     
-    typedef struct {
-        int dia, mes, ano;
-    } Data;
+typedef struct {
+    int dia, mes, ano;
+} Data;
 
-    typedef struct {
-        int codigo, numero_tlm, numero_filhos;
-        char nome[50];
-        Est_Civil est_civil;
-        Cargo cargo;
-        float valor_hora, valor_sub_ali;
-        Data nascimento, entrada_emp, saida_emp;
-        //falta o numero de horas
-    } Funcionario;
+typedef struct {
+    int codigo, numero_tlm, numero_filhos;
+    char nome[50];
+    Est_Civil est_civil;
+    Cargo cargo;
+    float valor_hora, valor_sub_ali;
+    Data nascimento, entrada_emp, saida_emp;
+    //falta o numero de horas
+    //integers para o processamento salarial
+    int dias_compl, dias_meios, dias_fds, dias_faltas; 
+} Funcionario;
 
 
 void lerData (Data *data);
