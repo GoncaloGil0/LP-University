a84e180dfd2758817b9adbbe201e0f36abe621ba
Gonçalo Gil <43255636+GoncaloGil0@users.noreply.github.com>
2020-12-09 15:18:00 +0000

    Add files via upload

---
+++ Headers/pedir_info.h
@@ -0,0 +1,9 @@
+// Codigo dedicado a todas as funções para pedir informação ao user 
+
+#ifndef PEDIR_INFO_H
+#define PEDIR_INFO_H
+
+int teste();
+
+#endif /* PEDIR_INFO_H */
+
---
+++ Headers/processar_info.h
@@ -0,0 +1,7 @@
+#ifndef PROCESSAR_INFO_H
+#define PROCESSAR_INFO_H
+
+int teste_process();
+
+#endif /* PROCESSAR_INFO_H */
+
