37adf39db4b8a089e33fc43ace6a6960b7c2f6bd
Gonçalo Gil <43255636+GoncaloGil0@users.noreply.github.com>
2020-12-10 16:40:41 +0000

    Update pedir_info.c

--- Source Files/pedir_info.c
+++ Source Files/pedir_info.c
@@ -10,25 +10,26 @@
     
     void addFuncionario (Funcionario *funcionario) {
         printf("\nCódigo: ");
-        scanf("%d", &funcionario->codigo);
+        scanf("%d", &funcionario->codigo); // se desse temos de fazer a verificação de que o input é um int e não uma string ou assim
         printf("\nNome: ");
-        scanf("%s", funcionario->nome);
+        scanf("%s", funcionario->nome); // se desse temos de fazer a verificação de que o input é uma string e não um int ou assim
         printf("\nNumero de telemovel: ");
-        scanf("%d", &funcionario->numero_tlm);
-        printf("\nEstado civil [0-Casado 1-Solteiro 2-Divorciado 3-Viúvo]: ");
-        scanf("%d", &funcionario->est_civil);
+        scanf("%d", &funcionario->numero_tlm); //temos de fazer a verificação se o numero é valido (ou seja, tenha 9 digitos)
+        printf("\nEstado civil [0-Casado 1-Solteiro 2-Divorciado 3-Viúvo]: "); 
+        scanf("%d", &funcionario->est_civil); //temos de fazer a verificação se o input nao é diferente daqueles 3
         printf("\nNumero de filhos no agregado familiar: ");
         scanf("%d", &funcionario->numero_filhos);
         printf("\nCargo na empresa [0-Empregado 1-Chefe 2-Administrador]: ");
-        scanf("%d", &funcionario->cargo);
+        scanf("%d", &funcionario->cargo);  //temos de fazer a verificação se o input nao é diferente daqueles 3
         printf("\nSalário por hora: ");
         scanf("%f", &funcionario->valor_hora);
         printf("\nValor do subsídio de alimentação: ");
         scanf("%f", &funcionario->valor_sub_ali);
         printf("\nData de nascimento (00.00.0000): ");
-        lerData(&funcionario->nascimento);
+        lerData(&funcionario->nascimento); //temos de fazer a verificação se a data e valida 
         printf("\nData de entrada na empresa (00.00.0000): ");
-        lerData(&funcionario->entrada_emp);
+        lerData(&funcionario->entrada_emp); //temos de fazer a verificação se a data e valida 
+        
         //(uma biblio que indique a saida da empresa) &funcionario->saida_emp;
         //falta o numero de horas
         // 
@@ -37,6 +38,8 @@
 
 
     void calcSalarial (Funcionario *funcionario) {
+        // temos de pedir e armazenar o mes
+        // e para cada data verificar se é valida e se os 4 parametros somados não dao superiores ao numero de dias nesse mes
         printf("\nNumero de dias completos trabalhados: ");
         scanf("%d", &funcionario->dias_compl);
         printf("\nNumero de meios dias trabalhados: ");
