7d8e149818366fbe43faaa4a458c80c0f5cca67f
sofs-00 <75382159+sofs-00@users.noreply.github.com>
2020-12-12 04:28:52 +0000

    Update main.c

--- Source Files/main.c
+++ Source Files/main.c
@@ -1,36 +1,32 @@
-/*
- *  Projeto de Laboratórios de Programação
- *      Programa de cálculo de salários
- */
-
-//Bibliotecas
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-//headers
 #include "pedir_info.h"
 #include "menus.h"
-#include "processar_info.h"
+//#include "processar_info.h"
+
+
 
 int main() {
-    
-    //Variaveis
+
+    //VARIAVEIS
     char resposta;
-    
-    //Registos
-    Calculo calculo;
-    
-    do{
-    menu();
-    
-    printf("Deseja continuar no programa [s/n]?\n");
-    scanf(" %c", &resposta);
-    
-    
+
+    //REGISTOS
+    //Calculo calculo;
+
+    //CHAMAR MENU
+    do {
+        menu();
+
+        printf("Deseja continuar no programa [s/n]?\n");
+        scanf(" %c", &resposta);
+
+
     } while (resposta != 'n');
-    
-    
+
+
     return (0);
 }
 
